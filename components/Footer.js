3d8c1d03b9ac818dc5722c52a8aaa9d816c1bbec
Taran234 <ts406093@gmail.com>
2024-03-22 13:20:20 -0400

    nav fix

--- components/Footer.js
+++ components/Footer.js
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
     <footer>
       <nav>
-        <a href="https://docs.frok.ai/introduction/readme" rel="noopener noreferrer" target="_blank" className="nav-link">Documentation<svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2.3rem" fill="currentColor" viewBox="0 0 256 256"><path d="M204,64V168a12,12,0,0,1-24,0V93L72.49,200.49a12,12,0,0,1-17-17L163,76H88a12,12,0,0,1,0-24H192A12,12,0,0,1,204,64Z"></path></svg></a>
+        <a href="https://docs.frok.ai/introduction/readme" rel="noopener noreferrer" target="_blank" className="nav-link">Documentation<svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" fill="currentColor" viewBox="0 0 256 256"><path d="M204,64V168a12,12,0,0,1-24,0V93L72.49,200.49a12,12,0,0,1-17-17L163,76H88a12,12,0,0,1,0-24H192A12,12,0,0,1,204,64Z"></path></svg></a>
         <a href="https://docs.frok.ai/introduction/readme" rel="noopener noreferrer" target="_blank" className="nav-link">TERMS & CONDITIONS
-          <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2.3rem" fill="currentColor" viewBox="0 0 256 256"><path d="M204,64V168a12,12,0,0,1-24,0V93L72.49,200.49a12,12,0,0,1-17-17L163,76H88a12,12,0,0,1,0-24H192A12,12,0,0,1,204,64Z"></path></svg></a>
-        <a href="https://docs.frok.ai/introduction/readme" rel="noopener noreferrer" target="_blank" className="nav-link">PRIVACY POLICY<svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2.3rem" fill="currentColor" viewBox="0 0 256 256"><path d="M204,64V168a12,12,0,0,1-24,0V93L72.49,200.49a12,12,0,0,1-17-17L163,76H88a12,12,0,0,1,0-24H192A12,12,0,0,1,204,64Z"></path></svg></a>
+          <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" fill="currentColor" viewBox="0 0 256 256"><path d="M204,64V168a12,12,0,0,1-24,0V93L72.49,200.49a12,12,0,0,1-17-17L163,76H88a12,12,0,0,1,0-24H192A12,12,0,0,1,204,64Z"></path></svg></a>
+        <a href="https://docs.frok.ai/introduction/readme" rel="noopener noreferrer" target="_blank" className="nav-link">PRIVACY POLICY<svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" fill="currentColor" viewBox="0 0 256 256"><path d="M204,64V168a12,12,0,0,1-24,0V93L72.49,200.49a12,12,0,0,1-17-17L163,76H88a12,12,0,0,1,0-24H192A12,12,0,0,1,204,64Z"></path></svg></a>
       </nav>
       <div>
         <FastMarquee
